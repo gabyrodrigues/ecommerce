1d504b78c90878a63249276b688de34ef853c1fe
Gabriely Rodrigues <gabriely.bleez@gmail.com>
2020-08-16 23:20:27 -0300

    ajustes css + js

---
+++ app/javascript/packs/checkout.js
@@ -0,0 +1,141 @@
+let body = document.querySelector("body");
+
+//funciona apenas no checkout para prevenir de exibir erros nas outras telas
+if(body.classList.contains("checkout")) {
+    //checkout wizard form
+    let currentTab = 0; // Current tab is set to be the first tab (0)
+    showTab(currentTab); // Display the current tab
+
+    let tab_wizard = document.getElementsByClassName("tab");
+
+    let next_button = document.getElementById("next_button");
+    let prev_button = document.getElementById("prev_button");
+
+    function showTab(n) {
+        let tab_wizard = document.getElementsByClassName("tab");
+
+        // This function will display the specified tab of the form...
+        tab_wizard[n].classList.add("-active");
+
+        let next_button = document.getElementById("next_button");
+        let prev_button = document.getElementById("prev_button");
+
+        //... and fix the Previous/Next buttons:
+        if (n == 0) {
+            prev_button.classList.remove("-visible");
+        } else {
+            prev_button.classList.add("-visible");
+        }
+
+        if (n == (tab_wizard.length - 1)) {
+            next_button.innerHTML = "Concluir compra";
+        } else {
+            next_button.innerHTML = "Próximo";
+        }
+        //... and run a function that will display the correct step indicator:
+        fixStepIndicator(n);
+    }
+
+    function nextPrev(n) {
+        // This function will figure out which tab to display
+        // Exit the function if any field in the current tab is invalid:
+        if (n == 1 && !validateForm()) return false;
+        // Hide the current tab:
+        tab_wizard[currentTab].classList.remove("-active");
+        // Increase or decrease the current tab by 1:
+        currentTab = currentTab + n;
+        // if you have reached the end of the form...
+        if (currentTab >= tab_wizard.length) {
+            // ... the form gets submitted:
+            // document.getElementById("regForm").submit();
+            return false;
+        }
+        // Otherwise, display the correct tab:
+        showTab(currentTab);
+    }
+
+    function validateForm() {
+        let card_payment = document.querySelector(".payment-forms .radio#credit");
+        let card_inputs = document.querySelectorAll(".card-container .fieldset .input");
+
+        // This function deals with validation of the form fields
+        let x, y, i, valid = true;
+
+        x = document.getElementsByClassName("tab");
+        y = x[currentTab].getElementsByTagName("input");
+
+        // A loop that checks every input field in the current tab:
+        for (i = 0; i < y.length; i++) {
+            // If a field is empty...
+            if (y[i].value == "") {
+
+                //removes the invalid class if other option is selected
+                if (!card_payment.checked) {
+                    for (let j = 0; j <= card_inputs.length; j++) {
+                        card_inputs[j].classList.remove("-invalid");
+                    }
+
+                    valid = true;
+                }
+
+                // add an "invalid" class to the field:
+                if (!y[i].classList.contains("-invalid")) {
+                    y[i].classList.add("-invalid");
+                }
+
+                // and set the current valid status to false
+                valid = false;
+            } else { //removing input invalid classes
+                if (y[i].classList.contains("-invalid")) {
+                    y[i].classList.remove("-invalid");
+                }
+            }
+        }
+
+        return valid; // return the valid status
+    }
+
+    function fixStepIndicator(n) {
+        // This function removes the "active" class of all steps...
+        let item_progress = document.querySelectorAll(".checkout-progress .item");
+
+        for (let i = 0; i < item_progress.length; i++) {
+            item_progress[i].classList.remove("-active");
+        }
+
+        //... and adds the "active" class on the current step:
+        item_progress[n].classList.add("-active");
+    }
+
+    prev_button.addEventListener('click', function () { nextPrev(-1) });
+    next_button.addEventListener('click', function () { nextPrev(1) });
+
+    //checkout hide/show payment forms
+    let payment_form = document.querySelectorAll(".payment-forms .radio");
+    let credit_payment = document.querySelector(".payment-forms .radio#credit");
+
+    let card_container = document.querySelector(".payment-container .card-container");
+    let boleto_container = document.querySelector(".payment-container .boleto-container");
+
+    for (let i = 0; i < payment_form.length; i++) {
+        payment_form[i].checked = true;
+
+        payment_form[i].onchange = function () {
+            if (credit_payment.checked) {
+                boleto_container.classList.remove("-visible");
+                card_container.classList.add("-visible");
+            } else {
+                boleto_container.classList.add("-visible");
+                card_container.classList.remove("-visible");
+                limpar_campos();
+            }
+        }
+    }
+
+    function limpar_campos() {
+        document.getElementById("numero_cartao").value = "";
+        document.getElementById("nome_titular").value = "";
+        document.getElementById("data_validade").value = "";
+        document.getElementById("cvv").value = "";
+    }
+}
--- app/javascript/packs/masks.js
+++ app/javascript/packs/masks.js
@@ -1,96 +1,101 @@
-function mascaraCartao(data) {
-    if (mascaraInteiro(data) == false) {
-        event.returnValue = false;
-    }
+let body = document.querySelector("body");
 
-    return formataCampo(data, '0000 0000 0000 0000', event);
-}
-
-function mascaraData(data) {
-    if (mascaraInteiro(data) == false) {
-        event.returnValue = false;
-    }
-
-    return formataCampo(data, '00/00', event);
-}
-
-function mascaraCvv(value) {
-    if (mascaraInteiro(value) == false) {
-        event.returnValue = false;
-    }
-
-    return formataCampo(value, '000', event);
-}
-
-function mascaraCep(value) {
-    if (mascaraInteiro(value) == false) {
-        event.returnValue = false;
-    }
-
-    return formataCampo(value, '00000-000', event);
-}
-
-//valida numero inteiro com mascara
-function mascaraInteiro() {
-    if (event.keyCode < 48 || event.keyCode > 57) {
-        event.returnValue = false;
-        return false;
-    }
-
-    return true;
-}
-
-//formata de forma generica os campos
-function formataCampo(campo, Mascara, evento) {
-    let boleanoMascara;
-
-    let digitado = evento.keyCode;
-    exp = /\-|\.|\/|\(|\)| /g
-    campoSoNumeros = campo.value.toString().replace(exp, "");
-
-    let posicaoCampo = 0;
-    let NovoValorCampo = "";
-    let TamanhoMascara = campoSoNumeros.length;;
-
-    if (digitado != 8) { // backspace
-        for (i = 0; i <= TamanhoMascara; i++) {
-            boleanoMascara = ((Mascara.charAt(i) == "-") || (Mascara.charAt(i) == ".")
-                || (Mascara.charAt(i) == "/"))
-            boleanoMascara = boleanoMascara || ((Mascara.charAt(i) == "(")
-                || (Mascara.charAt(i) == ")") || (Mascara.charAt(i) == " "))
-            if (boleanoMascara) {
-                NovoValorCampo += Mascara.charAt(i);
-                TamanhoMascara++;
-            } else {
-                NovoValorCampo += campoSoNumeros.charAt(posicaoCampo);
-                posicaoCampo++;
-            }
+//funciona apenas no checkout para prevenir de exibir erros nas outras telas
+if(body.classList.contains("checkout")) {
+    function mascaraCartao(data) {
+        if (mascaraInteiro(data) == false) {
+            event.returnValue = false;
         }
-        campo.value = NovoValorCampo;
+
+        return formataCampo(data, '0000 0000 0000 0000', event);
+    }
+
+    function mascaraData(data) {
+        if (mascaraInteiro(data) == false) {
+            event.returnValue = false;
+        }
+
+        return formataCampo(data, '00/00', event);
+    }
+
+    function mascaraCvv(value) {
+        if (mascaraInteiro(value) == false) {
+            event.returnValue = false;
+        }
+
+        return formataCampo(value, '000', event);
+    }
+
+    function mascaraCep(value) {
+        if (mascaraInteiro(value) == false) {
+            event.returnValue = false;
+        }
+
+        return formataCampo(value, '00000-000', event);
+    }
+
+    //valida numero inteiro com mascara
+    function mascaraInteiro() {
+        if (event.keyCode < 48 || event.keyCode > 57) {
+            event.returnValue = false;
+            return false;
+        }
+
         return true;
-    } else {
-        return true;
-    }
-}
-
-function id(el) {
-    return document.getElementById(el);
-}
-
-window.onload = function () {
-    id('cep').onkeypress = function () {
-        mascaraCep(this);
     }
 
-    id('cvv').onkeypress = function () {
-        mascaraCvv(this);
+    //formata de forma generica os campos
+    function formataCampo(campo, Mascara, evento) {
+        let boleanoMascara;
+
+        let digitado = evento.keyCode;
+        exp = /\-|\.|\/|\(|\)| /g
+        campoSoNumeros = campo.value.toString().replace(exp, "");
+
+        let posicaoCampo = 0;
+        let NovoValorCampo = "";
+        let TamanhoMascara = campoSoNumeros.length;;
+
+        if (digitado != 8) { // backspace
+            for (i = 0; i <= TamanhoMascara; i++) {
+                boleanoMascara = ((Mascara.charAt(i) == "-") || (Mascara.charAt(i) == ".")
+                    || (Mascara.charAt(i) == "/"))
+                boleanoMascara = boleanoMascara || ((Mascara.charAt(i) == "(")
+                    || (Mascara.charAt(i) == ")") || (Mascara.charAt(i) == " "))
+                if (boleanoMascara) {
+                    NovoValorCampo += Mascara.charAt(i);
+                    TamanhoMascara++;
+                } else {
+                    NovoValorCampo += campoSoNumeros.charAt(posicaoCampo);
+                    posicaoCampo++;
+                }
+            }
+            campo.value = NovoValorCampo;
+            return true;
+        } else {
+            return true;
+        }
     }
 
-    id('data_validade').onkeypress = function () {
-        mascaraData(this);
+    function id(el) {
+        return document.getElementById(el);
     }
 
-    id('numero_cartao').onkeypress = function () {
-        mascaraCartao(this);
+    window.onload = function () {
+        id('cep').onkeypress = function () {
+            mascaraCep(this);
+        }
+
+        id('cvv').onkeypress = function () {
+            mascaraCvv(this);
+        }
+
+        id('data_validade').onkeypress = function () {
+            mascaraData(this);
+        }
+
+        id('numero_cartao').onkeypress = function () {
+            mascaraCartao(this);
+        }
     }
 }
