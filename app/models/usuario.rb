06daecb090cd9f8542dd9b6fad953d223e767baf
Gabriely Rodrigues <gabriely.bleez@gmail.com>
2020-08-31 08:48:30 -0300

    cancelando conta

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -11,8 +11,22 @@ class Usuario < ApplicationRecord
     end
 
     after_create :insert_cliente
+    before_destroy :delete_user
 
     def insert_cliente
         Cliente.create(usuario_id: self.id)
+    end
+
+    def delete_user
+        cliente = Cliente.find_by(usuario_id: self.id)
+        admin = Admin.find_by(usuario_id: self.id)
+
+        if (cliente)
+            cliente.destroy
+        end
+
+        if (admin)
+            admin.destroy
+        end
     end
 end
