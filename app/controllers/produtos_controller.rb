84956bcd6077dd26a68a154f982b37ce2e4b18ad
Gabriely Rodrigues <gabriely.bleez@gmail.com>
2020-09-02 23:34:20 -0300

    cadastro de produtos (não finalizado)

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -18,53 +18,82 @@ class ProdutosController < ApplicationController
     # GET /produtos/1
     # GET /produtos/1.json
     def show
+        @categorias_produtos = CategoriasProduto.where(produto_id: @produto.id)
     end
 
     # GET /produtos/new
     def new
         @produto = Produto.new
+
+        @categorias = Categoria.all
     end
 
     # GET /produtos/1/edit
     def edit
+        @categorias = Categoria.all
     end
 
     # POST /produtos
     # POST /produtos.json
     def create
+        @categorias = params[:produto][:categoria]
         @produto = Produto.new(produto_params)
 
-        respond_to do |format|
-        if @produto.save
-            format.html { redirect_to @produto, notice: 'Produto was successfully created.' }
-            format.json { render :show, status: :created, location: @produto }
+        if !@categorias.blank? || @categorias != nil
+            respond_to do |format|
+                if @produto.save
+
+                    @categorias.each do |categoria|
+                        CategoriasProduto.create(categoria_id: categoria, produto_id: @produto.id)
+                    end
+
+                    format.html { redirect_to @produto, notice: 'Produto cadastrado com sucesso.' }
+                    format.json { render :show, status: :created, location: @produto }
+                else
+                    format.html { render :new }
+                    format.json { render json: @produto.errors, status: :unprocessable_entity }
+                end
+            end
         else
-            format.html { render :new }
-            format.json { render json: @produto.errors, status: :unprocessable_entity }
-        end
+            flash[:notice] =  'Selecione pelo menos uma categoria!'
+            redirect_to new_produto_path
         end
     end
 
     # PATCH/PUT /produtos/1
     # PATCH/PUT /produtos/1.json
     def update
-        # respond_to do |format|
-        #   if @produto.update(produto_params)
-        #     format.html { redirect_to @produto, notice: 'Produto was successfully updated.' }
-        #     format.json { render :show, status: :ok, location: @produto }
-        #   else
-        #     format.html { render :edit }
-        #     format.json { render json: @produto.errors, status: :unprocessable_entity }
-        #   end
-        # end
+        @categorias = params[:produto][:categoria]
+
+        if !@categorias.blank? || @categorias != nil
+            respond_to do |format|
+                if @produto.update(produto_params)
+                    format.html { redirect_to @produto, notice: 'Produto atualizado com sucesso.' }
+                    format.json { render :show, status: :ok, location: @produto }
+                else
+                    format.html { render :edit }
+                    format.json { render json: @produto.errors, status: :unprocessable_entity }
+                end
+            end
+        else
+            flash[:notice] =  'Selecione pelo menos uma categoria!'
+            redirect_to edit_produto_path
+        end
     end
 
     # DELETE /produtos/1
     # DELETE /produtos/1.json
     def destroy
+        @categorias_produtos = CategoriasProduto.where(produto_id: @produto.id)
+
+        @categorias_produtos.each do |categoria_produto|
+            categoria_produto.destroy
+        end
+
         @produto.destroy
+
         respond_to do |format|
-        format.html { redirect_to produtos_url, notice: 'Produto was successfully destroyed.' }
+        format.html { redirect_to produtos_url, notice: 'Produto deletado com sucesso.' }
         format.json { head :no_content }
         end
     end
@@ -72,11 +101,11 @@ class ProdutosController < ApplicationController
     private
         # Use callbacks to share common setup or constraints between actions.
         def set_produto
-        #@produto = Produto.find(params[:id])
+            @produto = Produto.find(params[:id])
         end
 
         # Only allow a list of trusted parameters through.
         def produto_params
-        params.require(:produto).permit(:nome, :preco, :quantidade)
+            params.require(:produto).permit(:nome, :preco, :quantidade, :foto)
         end
 end
--- app/models/produto.rb
+++ app/models/produto.rb
@@ -1,3 +1,5 @@
 class Produto < ApplicationRecord
-    validates_presence_of :nome, :preco, :quantidade
+    validates_presence_of :nome, :preco, :quantidade, :foto
+
+    has_one_attached :foto
 end
